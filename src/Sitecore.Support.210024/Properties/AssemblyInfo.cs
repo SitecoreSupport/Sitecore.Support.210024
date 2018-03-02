682b7a0e4d9ab688a6160e1eabe3203b0d22ee1d
Paul Stupka <psk@sitecore.net>
2018-03-02 18:08:19 +0200

    Fix #210024

--- src/Sitecore.Support.210024/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.210024/Properties/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.210024")]
 [assembly: AssemblyProduct("Sitecore.Support.210024")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Some segmentation rules do not work when Solr search provider is used")]
+
